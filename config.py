d81b87f85c52ae19c6a129dda524c05826728021
TASAWAR SARFRAZ <125805099+Tasawar-Sarfraz@users.noreply.github.com>
2026-09-07 19:35:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ def get_llm_instance(api_key: str = None):
         
     return ChatGroq(
         groq_api_key=key_to_use,
-        model_name="llama-3.3-70b-versatile",
+        model_name="llama-3.1-8b-instant",
         temperature=0.2
     )
